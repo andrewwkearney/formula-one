fbc24b77a19988887e516d487e41ddfc962dcee7
Andrew Kearney <andrewwkearney@gmail.com>
2019-04-27 16:09:52 +0100

    F1-5 Created the 25 point scoring system with fastest lap

--- hq/src/main/java/com/androsaces/formulaone/point/TwentyFivePointsAndFastestLap.java
+++ hq/src/main/java/com/androsaces/formulaone/point/TwentyFivePointsAndFastestLap.java
@@ -9,11 +9,12 @@ import java.util.Arrays;
 import java.util.Objects;
 
 /**
- * This points scoring system was used from 2010 until 2018, where points were
- * awarded to the top ten finishers in all races of the season, with the race
- * winner receiving 25 driver and constructor points down to the 10th place
- * finisher receiving 1 driver and constructor point. No points were awarded
- * to the pole position or fastest lap holder.
+ * This points scoring system was used from 2019, where points were awarded to
+ * the top ten finishers in all races of the season, with the race winner
+ * receiving 25 driver and constructor points down to the 10th place finisher
+ * receiving 1 driver and constructor point. Points were awarded to the fastest
+ * lap holder, provided they finished in the top ten. No points were awarded to
+ * the pole position holder.
  *
  * @author Andrew Kearney
  * @see FastestLap
@@ -21,8 +22,8 @@ import java.util.Objects;
  * @see ResultsCounted
  * @since 1.0
  */
-public class TwentyFivePoints implements ScoringSystem {
-    private final FastestLap mFastestLap = FastestLap.NOT_AWARDED;
+public class TwentyFivePointsAndFastestLap implements ScoringSystem {
+    private final FastestLap mFastestLap = FastestLap.AWARDED_TOP_TEN;
     private final PolePosition mPolePosition = PolePosition.NOT_AWARDED;
     private final ResultsCounted mPilotResultsCounted = ResultsCounted.ALL;
     private final ResultsCounted mConstructorResultsCounted = ResultsCounted.ALL;
@@ -54,18 +55,18 @@ public class TwentyFivePoints implements ScoringSystem {
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public int getConstructorPointsAwarded(int position) {
         Params.notNegative(position);
-        if (position > mConstructorPoints.length) {
+        if (position > mPilotPoints.length) {
             return 0;
         } else {
-            return mConstructorPoints[position - 1];
+            return mPilotPoints[position - 1];
         }
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof TwentyFivePoints)) return false;
-        TwentyFivePoints that = (TwentyFivePoints) o;
+        if (!(o instanceof TwentyFivePointsAndFastestLap)) return false;
+        TwentyFivePointsAndFastestLap that = (TwentyFivePointsAndFastestLap) o;
         return mFastestLap == that.mFastestLap &&
             mPilotResultsCounted == that.mPilotResultsCounted &&
             mConstructorResultsCounted == that.mConstructorResultsCounted &&
@@ -83,7 +84,7 @@ public class TwentyFivePoints implements ScoringSystem {
 
     @Override
     public String toString() {
-        return "TwentyFivePoints{" +
+        return "TwentyFivePointsAndFastestLap{" +
             "fastestLap=" + mFastestLap +
             ", polePosition=" + mPolePosition +
             ", pilotResultsCounted=" + mPilotResultsCounted +
