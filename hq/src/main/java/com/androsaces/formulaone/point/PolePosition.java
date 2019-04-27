9ab95bd612a3d371ad0278b416bf746fec7ba0ff
Andrew Kearney <andrewwkearney@gmail.com>
2019-04-27 15:49:09 +0100

    F1-5 Created a pole position enum

---
+++ hq/src/main/java/com/androsaces/formulaone/point/PolePosition.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019. Androsaces. All rights reserved.
+ */
+
+package com.androsaces.formulaone.point;
+
+import com.androsaces.formulaone.pilot.Pilot;
+
+/**
+ * This enumeration determines whether points were awarded to drivers who
+ * claimed pole position.
+ *
+ * @author Andrew Kearney
+ * @since 1.0
+ */
+public enum PolePosition {
+    /**
+     * Points were not awarded to the {@link Pilot} who started the race from
+     * pole position.
+     */
+    NOT_AWARDED
+}
