23cd464cb9020253b0ce11de8fc1487e6db81875
Andrew Kearney <andrewwkearney@gmail.com>
2019-04-27 15:49:34 +0100

    F1-5 Created a results counted enum

---
+++ hq/src/main/java/com/androsaces/formulaone/point/ResultsCounted.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019. Androsaces. All rights reserved.
+ */
+
+package com.androsaces.formulaone.point;
+
+/**
+ * This enumeration determines whether points were awarded to drivers or
+ * constructors for all or a selection of races in a season.
+ *
+ * @author Andrew Kearney
+ * @since 1.0
+ */
+public enum ResultsCounted {
+    /**
+     * Points counted in all races of the season.
+     */
+    ALL
+}
