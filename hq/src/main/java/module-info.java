7269d9f28e648473bf491fc5112b0443d9e941d1
Andrew Kearney <andrewwkearney@gmail.com>
2019-09-24 18:03:46 +0100

    Created module files for projects

---
+++ hq/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module hq {
+    requires org.slf4j;
+    requires com.androsaces.buckaroo;
+
+    exports com.androsaces.formulaone.pilot;
+    exports com.androsaces.formulaone.time;
+}
---
+++ pre-season/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module pre.season {
+    requires com.androsaces.buckaroo;
+    requires hq;
+
+    requires org.slf4j;
+    requires org.apache.logging.log4j;
+}
