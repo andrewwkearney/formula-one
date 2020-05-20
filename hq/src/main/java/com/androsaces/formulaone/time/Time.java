62b27578d7873e66a9ff2d2c99463806e623599c
Andrew Kearney <andrewwkearney@gmail.com>
2020-05-20 21:57:47 +0100

    F1-2 Added javadocs to the time interface

--- hq/src/main/java/com/androsaces/formulaone/time/Time.java
+++ hq/src/main/java/com/androsaces/formulaone/time/Time.java
@@ -1,14 +1,19 @@
 package com.androsaces.formulaone.time;
 
 /**
+ * This interface should be implemented by any class that represents a lap time,
+ * or some form of time within a Formula 1 session.
+ *
  * @author Andrew Kearney
  */
 public interface Time extends Comparable<Time> {
     /**
-     *
-     * @return
+     * Returns the time as a long.
      */
     long getTime();
 
+    /**
+     * Returns the time as a formatted string.
+     */
     String getTimeString();
 }
